be5ac70b1b4519efeae1d2df526ff124f056d48d
EmptyBox303 <167028233+EmptyBox303@users.noreply.github.com>
2026-05-22 16:45:28 -0500

    added utils file

--- src/content.js
+++ src/content.js
@@ -1,25 +1,4 @@
+import {ParseDomain} from "./utils.js";
+
 let currentURL = window.location.href;
-let l = currentURL.length;
-let i = 0;
-alert("switch tab");
-
-//O(n)
-for(i = 0; i < l-2; i++){
-  if(currentURL[i] === ':' && currentURL[i+1] === '/' && currentURL[i+2] === '/'){
-    break;
-  }
-}
-
-if(i === l-2){
-  throw new Error("url has no scheme\n");
-}
-
-let domain_start = i+3;
-//O(n) for length
-for(i = domain_start; i < l; i++){
-  if (currentURL[i] === '/' || currentURL[i] === ':')
-    break;
-}
-//domain_end is either the end of the URL or at index of first / or : after scheme
-let domain_end = i;
-let domain = currentURL.substring(domain_start,domain_end);
+let domain = ParseDomain(currentURL);
---
+++ src/utils.js
@@ -0,0 +1,24 @@
+export function ParseDomain(url){
+    let i = 0;
+    let l = url.length;
+    
+    //O(n)
+    for(i = 0; i < l-2; i++){
+        if(url[i] === ':' && url[i+1] === '/' && url[i+2] === '/'){
+            break;
+        }
+    }
+    if(i === l-2){
+        throw new Error("url has no scheme\n");
+    }
+    let domain_start = i+3;
+    for(i = domain_start; i < l; i++){
+    if (currentURL[i] === '/' || currentURL[i] === ':')
+        break;
+    }
+    //domain_end is either the end of the URL or at index of first / or : after scheme
+    let domain_end = i;
+    let domain = currentURL.substring(domain_start,domain_end);
+    alert(domain);
+    return domain;
+}
