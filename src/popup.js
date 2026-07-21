4ea19ae1d328dab736536eff38cb04b6acb86f6d
Jason Lu <JasonLu2027@u.northwestern.edu>
2026-07-21 12:04:30 -0500

    setup interactive html message thing

--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,24 @@
 var resetButton = document.getElementById("resetButton");
-
 var clearSession = document.getElementById("session");
 var clearCont = document.getElementById("continuous");
+var clearMinute = document.getElementById("minute");
+var clearHour = document.getElementById("hour");
+var clearDay = document.getElementById("day");
+var displayMessage = document.getElementById("display");
 
 resetButton.onclick = () =>{
+    displayMessage.innerText = ("this is working");
     //console.warn("this is happening");
-    chrome.storage.local.clear(() => {
+    /* chrome.storage.local.clear(() => {
         if (chrome.runtime.lastError){
             console.warn("Storage clear error:", chrome.runtime.lastError);
         }
         else{
             console.log("clear success");
         }
-    });
+    }); */
 
-    chrome.runtime.sendMessage({
+    /* chrome.runtime.sendMessage({
         clearRequest: true
-    });
+    }); */
 };
